f1874eac2de7788a5a6e7006af53a157ae5b49ec
Theo LEBRUN <mib.ftw@gmail.com>
2021-09-17 15:40:39 -0400

    Add matches number

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from bs4 import BeautifulSoup
 DATE = '06/14/2021'
 ORDER_NUMBER = 1375222
 RESTAURANT_NUMBER = '04161'
+MATCHES = 0
 
 cookie_jar = browser_cookie3.chrome(domain_name='.chick-fil-a.com')
 cookies = {'.AspNet.Cookies': cookie_jar._cookies['.chick-fil-a.com']['/']['.AspNet.Cookies'].value,
            'Crn.TokenInformation': cookie_jar._cookies['.chick-fil-a.com']['/']['Crn.TokenInformation'].value}
+# cookies = {
+#     '.AspNet.Cookies': '',
+#     'Crn.TokenInformation': ''}
 
 while ORDER_NUMBER < 9999999:
     print(ORDER_NUMBER)
@@ -24,8 +28,10 @@ while ORDER_NUMBER < 9999999:
     html = BeautifulSoup(r.text, 'html.parser')
     if html.find_all('h3', string='Success!'):
         print('MATCH FOUND, FREE POINTS!')
+        MATCHES += 1
     elif 'missed' in html.find_all('p', attrs={'role': 'alert'})[0].text:
         print('QUOTA REACHED')
+        print(f'MATCHES : {MATCHES}')
         break
     else:
         print('NO MATCH FOUND')
